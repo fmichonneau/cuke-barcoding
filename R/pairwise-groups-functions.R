fb2cb739451f4c907035b1b7d31a36ef1a1ee9a8
Francois Michonneau <francois.michonneau@gmail.com>
2014-08-06 16:17:46 -0400

    moved functions to find pairwise groups to separate file

---
+++ R/pairwise-groups-functions.R
@@ -0,0 +1,59 @@
+pairwiseGrps <- function(d, threshold) {
+    iGrp <- lapply(1:ncol(d), function(j) dimnames(d)[[1]][d[, j] < threshold])
+    sngl <- sapply(iGrp, function(x) length(x) == 1)
+    edg <- do.call("rbind", lapply(iGrp, function(x) {
+        if (length(x) > 1) cbind(head(x, -1), tail(x, -1)) else NULL
+    }))
+    g <- graph.data.frame(edg)
+    c(split(V(g)$name, clusters(g)$membership), iGrp[sngl])
+}
+
+getPairwiseGrp <- function(alg=cukeAlg, model=c("raw", "K80"),
+                           db=cukeDB,
+                           taxonomySubset, threshold=thresVec,
+                           taxonomyData=taxonomyDf) {
+    model <- match.arg(model)
+    if (missing(taxonomySubset) || taxonomySubset == "all")
+        distAlg <- ape::dist.dna(alg, model=model, as.matrix=TRUE)
+    else {
+        taxLvl <- taxonomyData[taxonomyData$taxa == taxonomySubset, "rank"]
+        if (length(taxLvl) > 1)
+            stop("Something is wrong with this taxonomic name.")
+        if (taxLvl == "Order") {
+            distLbl <- subset(db, order == taxonomySubset)$Labels_withAmb
+
+        } else if (taxLvl == "Family") {
+            distLbl <- subset(db, family == taxonomySubset)$Labels_withAmb
+
+        }
+        else stop("Houston, we have a problem.")
+        distLbl <- gsub("\\\"", "", distLbl)
+        indexLbl <- match(distLbl, dimnames(alg)[[1]])
+        stopifnot(! any(is.na(indexLbl)))
+        stopifnot(length(indexLbl) > 3)
+        distAlg <- dist.dna(alg[indexLbl, ], model=model, as.matrix=TRUE)
+    }
+    lapply(threshold, function(thres) pairwiseGrps(distAlg, thres))
+}
+
+getPropSngl <- function(x) {
+    tt <- sapply(x, length)
+    sum(tt == 1)/length(tt)
+}
+
+getPairwiseGrpRes <- function() {
+    pairwiseGrpRes <- vector("list", length(pairwiseGrpMdl) * length(pairwiseGrpTax))
+    nmGrpRes <- character(length(pairwiseGrpMdl) * length(pairwiseGrpTax))
+    i <- 1
+    for (eachMdl in 1:length(pairwiseGrpMdl)) {
+        for (eachTax in 1:length(pairwiseGrpTax)) {
+            pairwiseGrpRes[[i]] <- getPairwiseGrp(model=pairwiseGrpMdl[eachMdl],
+                                                  taxonomySubset=pairwiseGrpTax[eachTax])
+            nmGrpRes[i] <- paste(pairwiseGrpMdl[eachMdl], pairwiseGrpTax[eachTax], sep="-")
+            i <- i + 1
+        }
+    }
+    names(pairwiseGrpRes) <- nmGrpRes
+    pairwiseGrpRes
+}
+
