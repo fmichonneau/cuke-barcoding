04b35cd51f5ca207281c11854641a9e78338995c
Francois Michonneau <francois.michonneau@gmail.com>
2014-08-09 16:03:26 -0400

    adding code+data for isthmus analysis

---
+++ R/isthmus.R
@@ -0,0 +1,16 @@
+
+build_isthmus_alg <- function() {
+    isthmusId <- read.table(file="data/isthmus-id.txt", header=FALSE)
+    cukeAlg <- load_cukeAlg()
+    mtchId <- lapply(isthmusId[, 1], function(x) {
+        grep(paste0("_", x, "(_|$)"), dimnames(cukeAlg)[[1]])
+    })
+    lId <- sapply(mtchId, length)
+    stopifnot(identical(as.character(isthmusId[which(lId > 1), 1]),
+                        "UF9650"))
+    stopifnot(all(lId != 0))
+    mtchId <- sapply(mtchId, function(x) x[1])
+    isthmusAlg <- cukeAlg[mtchId, ]
+    ape::write.dna(isthmusAlg, file="data/cukeBarcodes-isthmus.phy",
+                   format="sequential", colsep="", colw=dim(cukeAlg)[2])
+}
