30b32c6fd830ba1487297d4008fffe0f81cde1d0
Francois Michonneau <francois.michonneau@gmail.com>
2016-06-06 19:45:41 -0400

    rename load_taxonomy_df --> load_taxonomy

--- R/load_taxonomy_df.R
+++ R/load_taxonomy_df.R
@@ -1,19 +1,24 @@
-load_taxonomy_df <- function(cukeDB) {
-    uniqOrder <- unique(cukeDB$order)
-    uniqFamily <- unique(cukeDB$family)
-    uniqFamily <- uniqFamily[! uniqFamily %in%
-                             c("Dactylochirotida", "?", "Uncertain")]
-    uniqTaxa <- c(uniqOrder, uniqFamily)
-    stopifnot(! any(duplicated(uniqTaxa)))
-    taxonomyDf <- data.frame(rank = c(rep("Order", length(uniqOrder) + 1),
-                                      rep("Family", length(uniqFamily))),
-                             taxa = c("all", uniqOrder, uniqFamily))
-    stopifnot(! any(duplicated(taxonomyDf$taxa)))
-    testFamily <- as.matrix(xtabs(~ family + order, data=cukeDB,
-                                  subset = !family %in% c("Dactylochirotida", "?", "Uncertain")))
-    whichOrder <- apply(testFamily, 1, function(x) which(x != 0))
-    tmpFamily <- data.frame(taxa = names(whichOrder),
-                            higher=dimnames(testFamily)[[2]][whichOrder])
-    taxonomyDf <- merge(taxonomyDf, tmpFamily, all.x=TRUE)
-    taxonomyDf
+## returns a "tidy" data frame of the classification for the families
+## and orders included in the database. The column names are taxa,
+## rank, higher.
+
+load_taxonomy <- function(cuke_db) {
+    uniq_order <- unique(cuke_db$order)
+    uniq_family <- unique(cuke_db$family)
+    uniq_family <- uniq_family[! uniq_family %in%
+                               c("Dactylochirotida", "?", "Uncertain")]
+    uniq_taxa <- c(uniq_order, uniq_family)
+    stopifnot(! any(duplicated(uniq_taxa)))
+    taxonomy_df <- data.frame(rank = c(rep("Order", length(uniq_order) + 1),
+                                       rep("Family", length(uniq_family))),
+                              taxa = c("all", uniq_order, uniq_family))
+    stopifnot(! any(duplicated(taxonomy_df$taxa)))
+    test_family <- as.matrix(xtabs(~ family + order, data = cuke_db,
+                                   subset = !family %in%
+                                       c("Dactylochirotida", "?", "Uncertain")))
+    which_order <- apply(test_family, 1, function(x) which(x != 0))
+    tmp_family <- data.frame(taxa = names(which_order),
+                             higher = dimnames(test_family)[[2]][which_order])
+    taxonomy_df <- merge(taxonomy_df, tmp_family, all.x = TRUE)
+    taxonomy_df
 }
