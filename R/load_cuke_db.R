fef39c6f30243620f1321fb1987f2c14a5100e6d
Francois Michonneau <francois.michonneau@gmail.com>
2016-07-03 09:23:45 -0400

    better checks on data

--- R/load_cuke_db.R
+++ R/load_cuke_db.R
@@ -13,6 +13,8 @@ load_echino_db <- function(cuke_file) {
 load_cuke_db <- function(echino_db) {
 
     cuke_db <- subset(echino_db, class == "Holothuroidea")
+
+    ## remove samples that don't pass the sequence checks
     cuke_db <- subset(cuke_db, ! pass.seq %in%  c("GenBank",
                                                   "fix",
                                                   "no_seq_yet",
@@ -22,16 +24,55 @@ load_cuke_db <- function(echino_db) {
     cuke_db <- subset(cuke_db, Notes != "MH sequence")
     cuke_db <- cuke_db[nzchar(cuke_db$Sample), ]
 
+
+    ## check for duplicated samples
+    dup <- cuke_db[duplicated(cuke_db$Sample), "Sample"]
+    if (length(dup) > 0) {
+        stop("There are duplicated samples: ",
+             paste(dup, collapse = ", "))
+    }
+
+    ## check pass:voucher column: no blank, no NAs, check values
+    if (length(nzchar(cuke_db$Pass.voucher)) != nrow(cuke_db)) {
+        stop("There are blank values in Pass.voucher column")
+    }
+    if (any(is.na(cuke_db$Pass.voucher))) {
+        stop("There are missing vlaues in Pass.voucher column")
+    }
+    allowed_pass_voucher_values <- c("yes", "no", "check/no", "check", "need",
+                                     "not ours", "catalog tissue?")
+    if (any(!cuke_db$Pass.voucher %in% allowed_pass_voucher_values)) {
+        stop("Only values allowed in pass:voucher column: ",
+             paste(allowed_pass_voucher_values, collapse = ", "),
+             ". Found: ",
+             paste(cuke_db$Pass.voucher[!cuke_db$Pass.voucher %in%
+                                        allowed_pass_voucher_values],
+                   collapse = ", "))
+    }
+
+    ## no empty values in repository
+    if (length(nzchar(cuke_db$Repository)) !=
+        nrow(cuke_db) |
+        any(is.na(cuke_db$Repository)))
+        stop("Blank values in Repository column")
+
     ## Taxonomic check
     test_genera <- as.matrix(xtabs(~ genusorhigher + family, data = cuke_db,
                                    subset = family != "Uncertain"))
     res_genera <- apply(test_genera, 1, function(x) sum(x != 0))
-    stopifnot(all(res_genera == 1))
+    if(any(res_genera != 1)) {
+        stop("These genera have more than 1 family assigned: ",
+             paste(names(res_genera[res_genera != 1]), sep = ", "))
+    }
 
     test_family <- as.matrix(xtabs(~ family + order, data = cuke_db,
                                    subset = family != "Uncertain"))
     res_family <- apply(test_family, 1, function(x) sum(x != 0))
-    stopifnot(all(res_family == 1))
+    if (any(res_family != 1)) {
+        stop("These families have more than 1 order assigned: ",
+             paste(names(res_family[res_family != 1]),
+                   sep = ", "))
+    }
 
     ## only non-ambiguous bp
     #l_seq <- sapply(cuke_db$Sequence, function(x)
@@ -44,9 +85,6 @@ load_cuke_db <- function(echino_db) {
     ## this also takes care of empty sequences (only -)
     cuke_db <- cuke_db[l_amb >= 500, ]
 
-    ## check for duplicated samples
-    dup <- cuke_db[duplicated(cuke_db$Sample), "Sample"]
-    stopifnot(length(dup) == 0)
 
     ## These 3 sequences are not represented by other representative
 
