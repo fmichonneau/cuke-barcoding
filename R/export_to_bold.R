016a901dfee08e3b16c9133c26642147e8a5bd7f
Francois Michonneau <francois.michonneau@gmail.com>
2016-07-22 18:52:21 -0400

    add functions to export data for BOLD submission

--- R/export_to_bold.R
+++ R/export_to_bold.R
@@ -49,13 +49,25 @@ lookup_repository_table <- function(code) {
     res
 }
 
-#lookup_collection_id_table <- function()
 
+bold_sample_id <- function(cuke_db) {
+    museum_id <- bold_museum_id(cuke_db)
+    paste(museum_id, substr(cuke_db$guid, 1, 8),
+          sep = "_")
+}
+
+bold_museum_id <- function(cuke_db) {
+    paste(cuke_db$Repository,
+          cuke_db$collection_code,
+          ifelse(cuke_db$Repository == "UF",
+                 paste(cuke_db$Catalog_number,
+                       "Echinodermata", sep = "-"),
+                 cuke_db$Catalog_number),
+          sep = ":")
+}
 
 bold_voucher_table <- function(cuke_db) {
 
-    stopifnot(!any(duplicated(cuke_db$Sample)))
-    cuke_db <- cuke_db[cuke_db$Pass.voucher == "yes", ]
     n_row <- nrow(cuke_db)
 
      res <- list(
@@ -66,16 +78,8 @@ bold_voucher_table <- function(cuke_db) {
         `Institution Storing` = character(n_row)
      )
 
-    museum_id <- paste(cuke_db$Repository,
-                       cuke_db$collection_code,
-                       ifelse(cuke_db$Repository == "UF",
-                              paste(cuke_db$Catalog_number,
-                                    "Echinodermata", sep = "-"),
-                                 cuke_db$Catalog_number),
-                              sep = ":")
-
-    sample_id <- paste(museum_id, substr(cuke_db$guid, 1, 8),
-                       sep = "_")
+    museum_id <- bold_museum_id(cuke_db)
+    sample_id <- bold_sample_id(cuke_db)
 
     res[["Sample ID"]] <- sample_id
     res[["Museum ID"]] <- museum_id
@@ -83,6 +87,144 @@ bold_voucher_table <- function(cuke_db) {
     res[["Collection Code"]] <- cuke_db$collection_code
     res[["Institution Storing"]] <- lookup_repository_table(cuke_db$Repository)
 
-    as.data.frame(res, stringsAsFactors = FALSE)
+    as.data.frame(res, stringsAsFactors = FALSE, check.names = FALSE)
 
+}
+
+
+bold_taxonomy_table <- function(cuke_db) {
+     n_row <- nrow(cuke_db)
+
+     res <- list(
+         `Sample ID` = bold_sample_id(cuke_db),
+         `Phylum ID` = rep("Echinodermata", n_row),
+         `Class` = cuke_db$class,
+         `Order` = cuke_db$order,
+         `Family` = cuke_db$family,
+         `Subfamily` = character(n_row),
+         `Genus` = cuke_db$genusorhigher,
+         `Species` = cuke_db$species
+     )
+     as.data.frame(res, stringsAsFactors = FALSE, check.names = FALSE)
+
+}
+
+bold_collection_table <- function(cuke_db) {
+
+    n_row <- nrow(cuke_db)
+
+    res <- list(
+        `Sample ID` = bold_sample_id(cuke_db),
+	`Collectors` = character(n_row),
+	`Collection Date` = character(n_row),
+	`Country/Ocean` = cuke_db[["Country/Ocean"]],
+        `State/Province` = character(n_row),
+        `Region` = character(n_row),
+        `Sector` = character(n_row),
+        `Exact Site` = character(n_row),
+        `Latitude` = cuke_db[["decimalLatitude"]],
+        `Longitude` =  cuke_db[["decimalLongitude"]],
+        `Elevation` = character(n_row)
+    )
+
+    if (any(is.na(res[["Country/Ocean"]])))
+        stop("Missing values in ", sQuote("Country/Ocean"), " for ",
+             paste(res[["Sample ID"]][is.na(res[["Country/Ocean"]])],
+                   collapse = ", \n"))
+
+    as.data.frame(res, stringsAsFactors = FALSE, check.names = FALSE)
+}
+
+
+write_bold_csv <- function(res, which_table, path = ".") {
+    fnm <- file.path(path, "bold_uploads",
+                     paste0(format(Sys.Date(), "%Y%m%d"),
+                            "-", which_table, ".csv"))
+    write_csv_(res, fnm)
+}
+
+write_csv_ <- function(res, fnm) {
+    res <- lapply(res, function(x) {
+        x[which(is.na(x))] <- ""
+        x
+    })
+    write.csv(data.frame(res, check.names = FALSE, stringsAsFactors = FALSE),
+              row.names = FALSE,
+              file = fnm
+              )
+    fnm
+}
+
+generate_bold_sequences <- function(cuke_db) {
+    cuke_seqs <- data.frame(
+        seq_ids = bold_sample_id(cuke_db),
+        seqs = gsub("-", "", cuke_db$Sequence)
+    )
+    fnm <- file.path("bold_uploads",
+                     paste0(format(Sys.Date(), "%Y%m%d"),
+                            "-", "bold-sequences.fas"))
+    generate_unaligned_cuke_fasta(cuke_seqs,
+                                  out = fnm)
+    fnm
+}
+
+
+generate_bold_data <- function(cuke_db) {
+    if (!exists("Country/Ocean", cuke_db)) {
+        stop("The cuke_db object is the target add_geodata")
+    }
+    stopifnot(!any(duplicated(cuke_db$Sample)))
+    cuke_db <- cuke_db[cuke_db$Pass.voucher == "yes" &
+                       (!is.na(cuke_db$decimalLatitude) |
+                        !is.na(cuke_db$decimalLongitude)), ]
+
+
+    bold_data <- lapply(list(
+        bold_voucher_table,
+        bold_taxonomy_table,
+        bold_collection_table
+    ), function(f) f(cuke_db))
+
+    res <- mapply(function(tbl, nm) {
+        write_bold_csv(tbl, nm)
+    }, bold_data, c("voucher", "taxonomy", "collection"))
+
+    seq <- generate_bold_sequences(cuke_db)
+    c(unlist(res), seq)
+}
+
+
+
+subset_bold_data <- function(date, sample_to_keep) {
+    csv_files <- paste0(date, "-",
+                       c("collection",
+                         "taxonomy",
+                         "voucher"), ".csv")
+    csv_files <- file.path("bold_uploads", csv_files)
+
+    if (!all(file.exists(csv_files))) {
+        stop("Some files are missing: ",
+             paste(csv_files[! file.exists(csv_files)],
+                   collapse = ", "))
+    }
+
+    res <- lapply(csv_files, function(x) {
+        tmp_csv <- read.csv(file = x, stringsAsFactors = FALSE,
+                            check.names = FALSE)
+        tmp_csv[match(sample_to_keep, tmp_csv$"Sample ID"), ]
+    })
+
+    subset_csv_files <- gsub("-", "-subset-", csv_files)
+
+    fnm <- mapply(function(x, y) {
+        write_csv_(x, y)
+    }, res, subset_csv_files)
+
+    alg_in <- file.path("bold_uploads", paste0(date, "-bold-sequences.fas"))
+    alg <- ape::read.dna(file = alg_in, format = "fasta", as.character = TRUE)
+    alg <- alg[sample_to_keep]
+    alg_sub <- gsub("([0-9]{8})-", "\\1-subset-", alg_in)
+    alg <- cbind(names(alg),  sapply(alg, function(x) paste(x, collapse = "")))
+    generate_unaligned_cuke_fasta(alg, out = alg_sub)
+    c(fnm, alg_sub)
 }
