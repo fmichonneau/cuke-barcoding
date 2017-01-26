ff1eb72693398652e7ab3b1ff8c2d5397dfdda9b
Francois Michonneau <francois.michonneau@gmail.com>
2017-01-26 17:15:00 -0500

    validate formatting of manual ESUs, and fix #4

--- R/esu_stats.R
+++ R/esu_stats.R
@@ -15,6 +15,8 @@ if (FALSE) {
     ## To create the backbone of the spreadsheet used to encode the ESUs manually:
     st <- storr::storr_rds("data/storr_clusters/")
     hol_tree <- get_tree_cluster_group(st, "Holothuriidae", "raw", 0.02, fetch("taxonomy"))
+    ## contamination,  dropped from current version of cuke_db
+    hol_tree <- phylobase::prune(hol_tree, tips.exclude = "9c18bc7a-bb03-4d33-b446-09328fb03193")
     esus <- tdata(hol_tree, "tip")
     esus$labels <- make_labels_from_guids(cuke_db, rownames(esus))
     esus$guids <- rownames(esus)
@@ -22,10 +24,24 @@ if (FALSE) {
     esus <- esus[order(esus$Groups), c("guids", "labels", "Groups")]
     write.csv(esus, file="data/raw/backbone_manualESUs.csv", row.names=FALSE)
     ## And then to plot the tree to make informed decision about the ESUs:
-    tipLabels(hol_tree) <- paste(make_labels_from_guids(fetch("cuke_db"), tipLabels(hol_tree)), tdata(hol_tree, "tip")$Groups, sep = "_")
+    tipLabels(hol_tree) <- paste(make_labels_from_guids(fetch("cuke_db"),
+                                                        tipLabels(hol_tree),
+                                                        fields = c(
+                                                            "family",
+                                                            "genusorhigher",
+                                                            "species",
+                                                            "modifier",
+                                                            "Loc",
+                                                            "Repository",
+                                                            "Catalog_number",
+                                                            "Sample"
+                                    )),
+                                 tdata(hol_tree, "tip")$Groups, sep = "_")
     hol_tr <- as(hol_tree, "phylo")
+    write.tree(hol_tr, file = "tmp/20170621.hol_tree.tre")
+
     pdf(file = "tmp/holothuriidae_raw_002_with_groups.pdf", height = 50)
-    ggtree(hol_tree) + geom_tiplab()
+    ggtree(hol_tree) #+ geom_tiplab()
     dev.off()
 
 
@@ -40,9 +56,66 @@ if (FALSE) {
     ##   - If different ESUs but not sisters: hol_ver_1 and hol_ver_2
 }
 
+validate_manual_esus <- function(manual_esu = "data/raw/edited_manualESUs.csv",
+                                 esu_name_table = "data/raw/ESU_names.csv") {
+
+    ## lst: list obtained with the split() function
+    ## msg: the message to show if the condition is triggered
+    ## f: stop, message or warning
+    check_group_counts <- function(lst, msg, f, esu_tbl) {
+        res <- vapply(lst, function(x) length(unique(x$Groups)), integer(1))
+        if (any(res > 1))
+            f(paste0(msg, ": \n  "),
+              paste(convert_esu_abbr(names(res)[res > 1], esu_tbl), collapse = "\n  "))
+    }
+
+    convert_esu_abbr <- function(nm, esu_tbl) {
+        res <- gsub("(.+)_(.+)_(.+)?", "\\1_\\2", nm)
+        paste(nm, esu_tbl[match(res, esu_tbl[, 1]), 2], sep = ": ")
+    }
+
+    man_esu <- read.csv(file = manual_esu, stringsAsFactors = FALSE)
+
+    ## validate that all ESU abbreviations are found in the ESU_names spreadsheet
+    esu_tbl <- read.csv(file = esu_name_table, header = FALSE, stringsAsFactors = FALSE)
+    stopifnot(any(duplicated(esu_tbl[, 1])))
+    esu_nm <- strsplit(unique(c(man_esu$ESU_genetic,
+                                man_esu$ESU_morphology,
+                                man_esu$ESU_taxonomy)), "_")
+    esu_nm <- vapply(esu_nm, function(x) paste(x[1:2], collapse = "_"), character(1))
+    esu_nm <- unique(esu_nm)
+
+    if (! all(esu_tbl[, 1] %in% esu_nm))
+        stop("Missing from manual ESU table: \n  ",
+             paste(convert_esu_abbr(setdiff(esu_tbl[, 1], esu_nm), esu_tbl),
+                   collapse = "\n  "))
+    if (! all(esu_nm %in% esu_tbl[, 1]))
+        stop("Missing from ESU name table: \n  ",
+             paste(convert_esu_abbr(setdiff(esu_nm, esu_tbl[, 1]), esu_tbl),
+                   collapse = "\n "))
+
+
+
+    man_esu_split <- split(man_esu, man_esu$ESU_genetic)
+
+    ## check that all n. sp. are correctly numbered: each ESU is only
+    ## found in 1 group
+    man_esu_nsp <- man_esu_split[grep("_nsp_", names(man_esu_split))]
+    check_group_counts(man_esu_nsp, "Some n. sp. are found in more than 1 group", stop, esu_tbl)
+
+    ## Show the Groups/clusters that have more than one ESU. These
+    ## normally indicate oversplitting by the clustering method, but
+    ## could also reflect a mistake in the assignments
+    check_group_counts(man_esu_split, "ESUs split into more than 1 group", message, esu_tbl)
+
+    invisible(NULL)
+}
+
+
 
 load_man_esu <- function(file) {
     manESU <- read.csv(file=file, stringsAsFactors=FALSE)
+    validate_manual_esus(file)
     manESU$ESU_noGeo <- gsub("_[A-Z]{2}$", "", manESU$ESU_genetic)
     manESU <- manESU[-grep("\\d+amb$", manESU$Labels), ]
     manESU
